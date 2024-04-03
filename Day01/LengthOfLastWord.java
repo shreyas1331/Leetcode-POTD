010ac03ff5894b5cfabd2d85d8e01a32321c51a6
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2024-04-04 00:09:24 +0530

    day3

--- Day01/LengthOfLastWord.java
+++ Day01/LengthOfLastWord.java
@@ -3,7 +3,16 @@ package Day01;
 public class LengthOfLastWord {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		s=s.trim();
+        int cnt=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                cnt++;
+            }else {
+                break;
+            }
+        }
+        return cnt;
 
 	}
 
---
+++ Day03/WordSearch.java
@@ -0,0 +1,10 @@
+package Day03;
+
+public class WordSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

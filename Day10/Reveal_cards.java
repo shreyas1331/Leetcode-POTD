41b8eaf2133985997487407fb4a6ce416254f1d8
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2024-04-10 23:07:00 +0530

    day10

---
+++ Day10/Reveal_cards.java
@@ -0,0 +1,27 @@
+package Day10;
+import java.util.*;
+public class Reveal_cards {
+
+	public static void main(String[] args) {
+		int[] deck= {17,13,11,2,3,5,7};
+		System.out.println(deckRevealedIncreasing(deck));
+	}
+	public static int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        Queue<Integer> index = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            index.add(i);
+        }
+
+        Arrays.sort(deck);
+        int[] rst = new int[n];
+        for (int card:deck) {
+            rst[index.poll()] = card;
+            if (!index.isEmpty()) {
+                index.add(index.poll());
+            }
+        }
+        return rst;
+    }
+
+}

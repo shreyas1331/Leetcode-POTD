6495682a0fcc5c3f9b4b8c68d0319c7211642506
Shreyas Mahajan <shreyasmahajan93@gmail.com>
2026-08-25 22:48:23 +0530

    1927 question

--- Day41/leetcode.py
+++ Day41/leetcode.py
@@ -1,2 +1,28 @@
-hi÷
-checking if my commit is recorded on the contribution chart
+# 1927. Sum Game
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        lq = rq = 0
+        ls = rs = 0
+        n = len(num)
+        for i in range(n//2):
+            if num[i]=='?':
+                lq+=1
+            else:
+                ls+=int(num[i])
+        for i in range(n//2,n):
+            if num[i]=='?':
+                rq+=1
+            else:
+                rs+=int(num[i])
+        mn = min(lq,rq)
+        lq-=mn
+        rq-=mn
+        if (lq==0 and rq==0) and ls==rs:
+            return False
+        elif lq==0 and rq!=0:
+            if rq%2==0 and ls-rs==(rq//2)*9:
+                return False
+        elif rq==0 and lq!=0:
+            if lq%2==0 and rs-ls==(lq//2)*9:
+                return False
+        return True
